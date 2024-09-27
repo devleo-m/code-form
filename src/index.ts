bdf44b1fe361213d2e3c74b8d78b46c8c9fa16a6
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-27 01:25:21 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,9 @@ const app = express();
 const PORT = process.env.APP_PORT;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello World!!!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
