4700585ae7329cc2a82464a60a9d0301b2d69440
Leonardo <149954966+devleo-m@users.noreply.github.com>
2024-09-26 21:13:00 -0300

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,9 @@
+npm init -y
+
+npm install express sequelize pg pg-hstore
+npm install dotenv
+npm install --save-dev typescript @types/node @types/express @types/sequelize ts-node
+npm install --save-dev nodemon @types/bcryptjs @types/jsonwebtoken @types/body-parser
+npm install --save-dev sequelize-cli
+
+npx tsc --init
